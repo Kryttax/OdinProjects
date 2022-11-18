b8e62379fc94aa60f2ca35db817a43cf90b618dc
Kryttax <kryttax@gmail.com>
2022-11-18 16:52:26 +0100

    Working adding/deleting.

--- Library/js/script.js
+++ Library/js/script.js
@@ -3,11 +3,19 @@ const newBookBtn = document.querySelector("#newBookBtn");
 newBookBtn.addEventListener("click", () => (formModal.style.display = "block"));
 
 const formModal = document.getElementById("form-container");
-const closeForm = document.getElementsByTagName("span")[0];
+const closeForm = document.getElementById("close-form");
 closeForm.addEventListener("click", () => (formModal.style.display = "none"));
 
-const addBookBtn = document.querySelector('#addBtn');
-addBookBtn.addEventListener('click', addDummyBook);
+const addBookBtn = document.querySelector("#addBtn");
+addBookBtn.addEventListener("click", addDummyBook);
+
+function validateForm() {
+  let name = document.forms["form"]["title"].value;
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
 
 let library = [];
 
@@ -24,14 +32,37 @@ function populateLibrary() {
   library.push(new Book("Dune", "Herbert", "524", false));
   library.push(new Book("Lastima", "Ebron", "24", false));
   library.push(new Book("Kika", "Superb", "100", true));
+
+  renderLibrary();
+}
+
+function renderLibrary() {
+  const libContainer = document.getElementById("library-container");
+  const books = document.querySelectorAll(".book");
+  books.forEach((book) => libContainer.removeChild(book));
+
   library.forEach((item) => {
     addBookToLibrary(item);
   });
 }
+
 let book;
 function addDummyBook() {
-  book = new Book("Dune", "Herbert", "524", false);
+  event.preventDefault();
+
+  for (const el of document
+    .getElementById("form")
+    .querySelectorAll("[required]")) {
+    if (!el.reportValidity()) {
+      return;
+    }
+  }
+
+  formModal.style.display = "none";
+
+  book = new Book(form.title.value, form.author.value, form.pages.value, false);
   library.push(book);
+
   addBookToLibrary(book);
 }
 
@@ -42,9 +73,18 @@ function addBookToLibrary(item) {
   const titleDiv = document.createElement("div");
   const authorDiv = document.createElement("div");
   const pagesDiv = document.createElement("div");
+  const readDiv = document.createElement("div");
+  const closeBtn = document.createElement("span");
 
   bookDiv.classList.add("book");
   bookDiv.setAttribute("id", library.indexOf(item));
+
+  closeBtn.classList.add("close");
+  closeBtn.textContent = "X";
+  closeBtn.addEventListener("click", () =>
+    removeBookFromLibrary(library.indexOf(item))
+  );
+  bookDiv.appendChild(closeBtn);
 
   titleDiv.textContent = item.title;
   titleDiv.classList.add("title");
@@ -58,7 +98,16 @@ function addBookToLibrary(item) {
   pagesDiv.classList.add("info");
   bookDiv.appendChild(pagesDiv);
 
+  readDiv.textContent = item.read;
+  readDiv.classList.add("info");
+  bookDiv.appendChild(readDiv);
+
   libContainer.appendChild(bookDiv);
+}
+
+function removeBookFromLibrary(index) {
+  library.splice(index, 1);
+  renderLibrary();
 }
 
 populateLibrary();
