77fd76eeb7bf642f9debbc075a37e426ec15eb36
Kryttax <kryttax@gmail.com>
2022-11-29 16:24:40 +0100

    Restaurant finished.

--- Restaurant/src/pages/menu.js
+++ Restaurant/src/pages/menu.js
@@ -1,34 +1,61 @@
 import drink1_ic from "../assets/icons/davidMartinez.png";
 import drink2_ic from "../assets/icons/johnnySilverhand.png";
-const createDrink = (name, icon, ingredients) => {
+import drink3_ic from "../assets/icons/jackieWelles.png";
+import drink4_ic from "../assets/icons/joeTiels.png";
+
+const createDrink = (name, quote, icon, ingredients) => {
   const drink = document.createElement("span");
   drink.classList.add("drink");
+
+  const imgContainer = document.createElement("span");
+  imgContainer.classList.add("imgContainer");
   const ic = document.createElement("img");
   ic.classList.add("img");
   ic.src = icon;
-  drink.appendChild(ic);
+  imgContainer.appendChild(ic);
 
-  const divInfo = document.createElement("div");
-  divInfo.classList.add("infoContainer");
+  const infContainer = document.createElement("span");
+  infContainer.classList.add("infoContainer");
 
   const title = document.createElement("span");
   title.classList.add("info");
   title.textContent = name;
-  divInfo.appendChild(title);
+  infContainer.appendChild(title);
+
+  const subtitle = document.createElement("span");
+  subtitle.classList.add("quote");
+  subtitle.textContent = quote;
+  infContainer.appendChild(subtitle);
+
+  const ingContainer = document.createElement("span");
+  ingContainer.classList.add("ingrContainer");
+
+  const ingtitle = document.createElement("span");
+  ingtitle.classList.add("info");
+  ingtitle.textContent = "Ingredients";
+  ingContainer.appendChild(ingtitle);
 
   const ingredientsList = document.createElement("ul");
 
-
   for (let i = 0; i < ingredients.length; i++) {
     const ingredient = document.createElement("li");
+    ingredient.classList.add("ingredient");
     ingredient.appendChild(document.createTextNode(ingredients[i]));
     ingredientsList.appendChild(ingredient);
   }
 
-  divInfo.appendChild(ingredientsList);
-  drink.appendChild(divInfo);
+  ingContainer.appendChild(ingredientsList);
+  drink.appendChild(imgContainer);
+  drink.appendChild(infContainer);
+  drink.appendChild(ingContainer);
 
   return drink;
+};
+
+const addBanner = () => {
+  const banner = document.createElement("div");
+  banner.classList.add("banner");
+  return banner;
 };
 
 const menuPage = () => {
@@ -37,7 +64,7 @@ const menuPage = () => {
   element.classList.add("main");
 
   const title = document.createElement("span");
-  title.textContent = "MENU";
+  title.textContent = "DRINKS";
   title.classList.add("title");
 
   const cDiv = document.createElement("div");
@@ -49,31 +76,68 @@ const menuPage = () => {
   cDiv.appendChild(subtitle);
 
   const subtitle2 = document.createElement("span");
-  subtitle2.classList.add("subtitle");
-  subtitle2.textContent = "V V";
+  subtitle2.classList.add("accent");
+  subtitle2.textContent = "/ / /";
   cDiv.appendChild(subtitle2);
-
-  element.appendChild(title);
-  element.appendChild(cDiv);
 
   const divItems = document.createElement("div");
   divItems.classList.add("itemsContainer");
-  cDiv.appendChild(divItems);
+  // cDiv.appendChild(divItems);
 
-  // const drink1 = document.createElement("span");
-  // divItems.classList.add("drink");
-  // const drink1img = document.createElement("img");
-  // drink1img.src = drink1_ic;
-  // drink1.appendChild(drink1img);
+  const drink1 = createDrink(
+    "David Martinez",
+    "A shot of vodka on the rocks, topped with NiCola. Aim high and go out with a bang.",
+    drink1_ic,
+    [
+      "30 ml. Nicola Blue Syrup",
+      "15 ml. Nicola Classic Syrup",
+      "60 ml. Vodka",
+      "Few drops of food coloring",
+      "90 ml. Club Soda",
+      "Twist of lemon",
+      "Stir",
+      "Ice",
+    ]
+  );
 
-  // const drink1ing = document.createElement("ul");
-  // drink1ing.classList.add("drink");
+  const drink2 = createDrink(
+    "Jhonny Silverhand",
+    "A tequila old fashioned with a splash of beer and a chili garnish. For the rockerboy who never gave up.",
+    drink2_ic,
+    [
+      "60ml. aged tequila",
+      "1 barspoon agave nectar",
+      "2-3 dashes Angostura bitters",
+      "Orange twist",
+      "Chili pepper",
+      "Splash of Mexican beer (around 30ml.)",
+    ]
+  );
 
-  const drink1 = createDrink("David Martinez", drink1_ic, ["Acid", "Lemon"]);
-  const drink2 = createDrink("Jhonny Silverhand", drink2_ic, ["Acid", "Lemon"]);
-
+  const drink3 = createDrink(
+    "Jackie Welles",
+    "A shot of vodka, lime juice, ginger beer and, most importantly... a splash of love.",
+    drink3_ic,
+    ["2 oz of vodka", "5.5 ounces of ginger beer", "Orange Juice", "Lime"]
+  );
+  const drink4 = createDrink(
+    "Joe Tiel's Okie Hooch",
+    "A taste of Oklahoma.",
+    drink4_ic,
+    ["40ml. of tequila", "Green tea", "Tabasco"]
+  );
   divItems.appendChild(drink1);
+  divItems.appendChild(addBanner());
   divItems.appendChild(drink2);
+  divItems.appendChild(addBanner());
+  divItems.appendChild(drink3);
+  divItems.appendChild(addBanner());
+  divItems.appendChild(drink4);
+  divItems.appendChild(addBanner());
+
+  element.appendChild(title);
+  element.appendChild(cDiv);
+  element.appendChild(divItems);
 
   return element;
 };
